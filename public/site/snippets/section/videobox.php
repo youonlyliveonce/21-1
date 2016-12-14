400b0002c0db31ddadd8a8476971eac4fae59f02
danielkremin <design@kremin.de>
2016-12-14 23:42:30 +0100

    Add Video Loop

--- public/site/snippets/section/videobox.php
+++ public/site/snippets/section/videobox.php
@@ -3,7 +3,7 @@
 ?>
 <div class="Videobox [ Element ]" id="<?php echo $section->slug(); ?>"  data-view="VideoView">
 	<div class="Videobox__background">
-			<iframe width="420" height="315" src="https://www.youtube.com/embed/<?php echo $section->videoid(); ?>?autoplay=0&enablejsapi=1&loop=1&showinfo=0&controls=0&enablejsapi=1"></iframe>
+			<iframe width="420" height="315" src="https://www.youtube.com/embed/<?= $section->videoid(); ?>?autoplay=0&enablejsapi=1&loop=1&playlist=<?= $section->videoid(); ?>&showinfo=0&controls=0&enablejsapi=1"></iframe>
 	</div>
 	<div class="Videobox__body">
 
