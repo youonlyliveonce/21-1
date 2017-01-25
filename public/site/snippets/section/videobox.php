1e92858fdb76cd04896c8f14424067827b8b369e
danielkremin <design@kremin.de>
2017-01-25 22:22:35 +0100

    #27 fixed

--- public/site/snippets/section/videobox.php
+++ public/site/snippets/section/videobox.php
@@ -1,7 +1,16 @@
 <?php
-
+	$awards = $section->awards()->toStructure();
 ?>
 <div class="Videobox [ Element ]" id="<?php echo $section->slug(); ?>"  data-view="VideoView">
+	<?php if(count($awards) > 0): ?>
+		<div class="Videobox__awards">
+			<ul>
+			<?php foreach($awards as $award): ?>
+				<li><img src="<?= $award->image()->toFile()->url(); ?>"></li>
+			<?php endforeach; ?>
+			</ul>
+		</div>
+	<?php endif; ?>
 	<div class="Videobox__background">
 			<iframe width="420" height="315" src="https://www.youtube.com/embed/<?= $section->videoid(); ?>?autoplay=0&enablejsapi=1&loop=1&playlist=<?= $section->videoid(); ?>&showinfo=0&controls=0&enablejsapi=1"></iframe>
 	</div>
